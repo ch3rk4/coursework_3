370d5552f59e7876b171d09673b5df5f8836ea9e
ch3rk4 <pety.shurupov@gmail.com>
2025-06-30 17:54:17 +0300

    refresh models from users

--- users/models.py
+++ users/models.py
@@ -1,5 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Кастомный менеджер для модели пользователя.
+
+    Переопределяет методы создания пользователей для работы с email
+    вместо username в качестве основного поля аутентификации.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Создает и сохраняет обычного пользователя с email и паролем.
+        """
+        if not email:
+            raise ValueError('Email адрес обязателен')
+
+        email = self.normalize_email(email)
+
+        # Устанавливаем username равным email для совместимости
+        extra_fields.setdefault('username', email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Создает и сохраняет суперпользователя с email и паролем.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -43,6 +84,9 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     # Убираем email из REQUIRED_FIELDS, так как он уже указан как USERNAME_FIELD
     REQUIRED_FIELDS = []
+
+    # Подключаем кастомный менеджер
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
